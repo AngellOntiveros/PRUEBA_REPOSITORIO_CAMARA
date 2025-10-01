c8863bef6f9d9617415c48ef942a20e6cba18e42
AngellOntiveros <angelontiveros475@gmail.com>
2025-10-01 10:57:20 -0500

    Update PROYECTO STREAMLIT.py

--- PROYECTO STREAMLIT.py
+++ PROYECTO STREAMLIT.py
@@ -24,10 +24,10 @@ from io import BytesIO
 @st.cache_resource
 def descargar_y_cargar_modelo_frutas():
     """Descargar y cargar modelo de frutas"""
-    ruta_modelo = "W_FRUTA.pt" 
+    ruta_modelo = "CNN_FRUTA.pt" 
     if not os.path.exists(ruta_modelo):
         st.info("📥 Descargando modelo de frutas...") 
-        url = "https://drive.google.com/uc?id=1lfq0_VK9DZsgR-TVGraBrYDUNu-P-hTl" 
+        url = "https://drive.google.com/uc?id=13f5QbkwbR-SpQHqkb2tsEcondQMYKNnx" 
         gdown.download(url, ruta_modelo, quiet=False)
     
     return YOLO(ruta_modelo)
@@ -819,6 +819,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
